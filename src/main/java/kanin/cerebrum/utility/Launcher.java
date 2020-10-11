df9b880de86dbdd17a31ce13e3b9256eb210d56f
Albert <47435340+kaninvt@users.noreply.github.com>
2020-10-10 21:35:25 -0400

    Formatting

--- src/main/java/kanin/cerebrum/utility/Launcher.java
+++ src/main/java/kanin/cerebrum/utility/Launcher.java
@@ -2,5 +2,4 @@ package kanin.cerebrum.utility;
 
 import kanin.cerebrum.Main;
 
-public class Launcher {public static void main(String[] args){
-    Main.main(args);}}
+public class Launcher {public static void main(String[] args){ Main.main(args);}}
